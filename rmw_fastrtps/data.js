4f0ed47e0f2508d5776a2c6ba3f0f3a42fa1c8ca
github-action-benchmark <github@users.noreply.github.com>
2024-07-27 21:07:03 +0000

    add Perception Pipeline Benchmark (googlecpp) benchmark result for 97e6c87c1098cea161559bdf72bf837774d58cbd

--- rmw_fastrtps/data.js
+++ rmw_fastrtps/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722113075528,
+  "lastUpdate": 1722114423071,
   "repoUrl": "https://github.com/DarkusAlphaHydranoid/moveit_middleware_benchmark_experimental",
   "entries": {
     "Perception Pipeline Benchmark": [
@@ -232,6 +232,36 @@ window.BENCHMARK_DATA = {
             "value": 295405757580.00006,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 1997181259 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jarbay910@gmail.com",
+            "name": "DarkusAlphaHydranoid",
+            "username": "DarkusAlphaHydranoid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "97e6c87c1098cea161559bdf72bf837774d58cbd",
+          "message": "Provide benchmark reliability by conducting benchmark steps in same job (#12)\n\n* Add needs parameter to run_benchmarks CI\r\n\r\n* Update run_benchmarks.yml",
+          "timestamp": "2024-07-27T23:56:09+03:00",
+          "tree_id": "2a594bb38c0bde0e430ccdb7adac8f491357d12c",
+          "url": "https://github.com/DarkusAlphaHydranoid/moveit_middleware_benchmark_experimental/commit/97e6c87c1098cea161559bdf72bf837774d58cbd"
+        },
+        "date": 1722114422826,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 256419323374.99997,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1682403620 ns\nthreads: 1"
           }
         ]
       }
