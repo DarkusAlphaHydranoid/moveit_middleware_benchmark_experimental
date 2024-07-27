a4025bcc946e85ed3ab0ca345450f0d75bc816f3
github-action-benchmark <github@users.noreply.github.com>
2024-07-27 22:10:58 +0000

    add Basic Service Client Benchmark (googlecpp) benchmark result for 97e6c87c1098cea161559bdf72bf837774d58cbd

--- rmw_fastrtps/data.js
+++ rmw_fastrtps/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722118256626,
+  "lastUpdate": 1722118258323,
   "repoUrl": "https://github.com/DarkusAlphaHydranoid/moveit_middleware_benchmark_experimental",
   "entries": {
     "Perception Pipeline Benchmark": [
@@ -556,6 +556,36 @@ window.BENCHMARK_DATA = {
             "value": 2365058124.000001,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 892024214 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jarbay910@gmail.com",
+            "name": "DarkusAlphaHydranoid",
+            "username": "DarkusAlphaHydranoid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "97e6c87c1098cea161559bdf72bf837774d58cbd",
+          "message": "Provide benchmark reliability by conducting benchmark steps in same job (#12)\n\n* Add needs parameter to run_benchmarks CI\r\n\r\n* Update run_benchmarks.yml",
+          "timestamp": "2024-07-27T23:56:09+03:00",
+          "tree_id": "2a594bb38c0bde0e430ccdb7adac8f491357d12c",
+          "url": "https://github.com/DarkusAlphaHydranoid/moveit_middleware_benchmark_experimental/commit/97e6c87c1098cea161559bdf72bf837774d58cbd"
+        },
+        "date": 1722118257752,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ScenarioBasicServiceClientFixture/test_scenario_basic_service_client",
+            "value": 2515355342.999982,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 933908402 ns\nthreads: 1"
           }
         ]
       }
