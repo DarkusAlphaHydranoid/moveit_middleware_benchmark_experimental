a307549ac7b23568fca9462a154cc5a83a7eae27
github-action-benchmark <github@users.noreply.github.com>
2024-08-22 19:06:42 +0000

    add Basic Service Client Benchmark (googlecpp) benchmark result for b4e015c453f6d160ec4d2fb617b29252e457412f

--- rmw_cyclonedds/data.js
+++ rmw_cyclonedds/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724353601099,
+  "lastUpdate": 1724353602315,
   "repoUrl": "https://github.com/DarkusAlphaHydranoid/moveit_middleware_benchmark_experimental",
   "entries": {
     "Perception Pipeline Benchmark": [
@@ -788,6 +788,36 @@ window.BENCHMARK_DATA = {
             "value": 3140524298.999992,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 1117444533.9999998 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jarbay910@gmail.com",
+            "name": "DarkusAlphaHydranoid",
+            "username": "DarkusAlphaHydranoid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b4e015c453f6d160ec4d2fb617b29252e457412f",
+          "message": "Update run_benchmarks.yml",
+          "timestamp": "2024-08-22T21:55:41+03:00",
+          "tree_id": "240fba10181ac159d25f5511ec2d7396090ac316",
+          "url": "https://github.com/DarkusAlphaHydranoid/moveit_middleware_benchmark_experimental/commit/b4e015c453f6d160ec4d2fb617b29252e457412f"
+        },
+        "date": 1724353601975,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ScenarioBasicServiceClientFixture/test_scenario_basic_service_client",
+            "value": 3372814518.000041,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1194423639 ns\nthreads: 1"
           }
         ]
       }
